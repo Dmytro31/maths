f94cc6d9a73214e4f4fc7bf6c4a0d676328ba76c
Dmytro31 <dima321fedoruk@gmail.com>
2022-02-23 00:08:20 +0200

    load

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a= 25/3;
+        System.out.println("Рузультат : "+a);
+        a = 25%3;
+        System.out.println("Рузультат : "+a);
+        b = 25+45;
+        System.out.println("Рузультат : "+b);
+        double c = 734.4628;
+        System.out.println("Рузультат : "+(int) c);
+
+    }
+}
